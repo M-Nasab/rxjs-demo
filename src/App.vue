9d8983258626420f43d455ab7659cc7a01e41377
Mostafa Amininasab <m.amininasab@alibaba.ir>
2020-12-23 01:02:14 +0330

    create observable from array

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,75 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <h1 style="text-align: center;">
+      RxJS Demo
+    </h1>
+
+    <div class="data-container">
+      <ul>
+        <li v-for="(data, index) in list" :key="index">
+          {{ data }}
+        </li>
+      </ul>
     </div>
-    <router-view/>
+
   </div>
 </template>
 
+<script>
+import { from } from 'rxjs';
+
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  mounted() {
+    const observer = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    observer.subscribe((data) => {
+      this.list.push(data);
+    });
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+html, body {
+  font-family: 'Amatic SC', cursive;
+  margin: 0;
+  padding: 0;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+h1 {
+  font-size: 70px;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: flex-start;
+  width: 100%;
+  height: 100vh;
+  padding: 100px;
+  padding-top: 50px;
+}
+
+.data-container {
+  border: 4px solid black;
+  overflow: auto;
+  padding: 20px;
+}
+
+ul {
+  font-size: 40px;
+  margin: 0;
 }
 </style>
